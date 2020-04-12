e7fde10f88efeeaaef847cd5b539d4a669992f88
Allen Phillip Pottinger <allenpottinger@gmail.com>
2020-04-12 20:18:22 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,41 +1,17 @@
 require 'nokogiri'
 require 'open-uri'
 require 'pry'
+ 
 require_relative './course.rb'
-
+ 
 class Scraper
-  
-  html = open("http://learn-co-curriculum.github.io/site-for-scraping/courses")
-  
+ 
   def get_page
-    doc = Nokogiri::HTML(html)
-  end 
-  
-    binding.pry 
-  end 
-
-end 
-
-Scraper.new.get_page
-  
-  
-  
-  
-  
-  
-  
-  
-  def print_courses
-    self.make_courses
-    Course.all.each do |course|
-      if course.title && course.title != ""
-        puts "Title: #{course.title}"
-        puts "  Schedule: #{course.schedule}"
-        puts "  Description: #{course.description}"
-      end
-    end
-  end 
+      doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+ 
+      binding.pry
+  end
+ 
 end
-
-
-
+ 
+Scraper.new.get_page
